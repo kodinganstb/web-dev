567c8cfdb292d49b34927a17d1831cbb62559319
Agung Prasetyo <prazzid31@gmail.com>
2022-10-12 00:06:07 +0700

    upload function.js

---
+++ javascript/function.js
@@ -0,0 +1,17 @@
+// membuat function pada JavaScript
+function namaFunction(){
+    // disini property dan method
+}
+
+// contoh dibawah membuat function Motor
+function motor(){
+    let merk = "Yamahmud";
+    let cc = 125;
+    let tahunKeluar = 2015;
+    let harga = 15000000;
+
+    return `Sebuah motor ${merk}, keluaran tahun ${tahunKeluar} dijual dengan harg ${harga} dengan mesin ${cc}CC. BU Cepat!`
+
+}
+
+motor();
