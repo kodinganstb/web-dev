f57b70c1a7224111d003612004a8bf2d2d7a8d60
Agung Prasetyo <prazzid31@gmail.com>
2022-10-11 23:58:23 +0700

    upload javascript folder/*.js

---
+++ javascript/variable.js
@@ -0,0 +1,7 @@
+/* format membuat Variabel pada Javascript */
+/* menggunakan var / let / const, diikuti nama variabel = isi variabel, bisa string atau integer */
+/* contoh dibawah */
+
+var namaVariabel = "Hello World!";
+/* atau */
+var namaVariabel = 123;
