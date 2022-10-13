467f31d5e7380ba6066af4f896966172fca64bb5
Agung Prasetyo <prazzid31@gmail.com>
2022-10-13 15:30:44 +0700

    upload file switch-case-statment.js

---
+++ javascript/switch-case-statment.js
@@ -0,0 +1,22 @@
+let language = "French";
+let greeting = null;
+
+switch (language) {
+    case "English":
+        greeting = "Good Morning!";
+        break;
+    case "French":
+        greeting = "Bonjour!";
+        break;
+    case "Japanese":
+        greeting = "Ohayou Gozaimasu!";
+        break;
+    default:
+        greeting = "Selamat Pagi!";
+}
+
+console.log(greeting);
+
+/* output
+Bonjour!
+*/
